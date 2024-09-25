ed1a955288b963223d2e9f190afc7dcee060a9c5
Hagen <hagenotto23@icloud.com>
2024-09-25 14:39:19 +0200

    Update 4_9_2024.py

--- 4_9_2024.py
+++ 4_9_2024.py
@@ -1,1 +1,25 @@
 from random import randint
+
+Zahl = 1
+Kopf = 0
+
+while True:
+    eingabe = input("Bitte gegen sie die Variablen für Kopf (k) und Zahl (z) ein:").upper
+    if eingabe == "K" or "Z":
+        break
+
+zufalsZahl = randint(0,1)
+
+if zufalsZahl == 1:
+    print("Zahl hat gewonnen")
+    seite = "Z"
+if zufalsZahl == 0:
+    print("Kopf hat gewonnen")
+    seite = "K"
+
+if eingabe != seite:
+    win = "nicht"
+else:
+    win = ""
+
+print("Sie haben " + win + " gewonnen")
