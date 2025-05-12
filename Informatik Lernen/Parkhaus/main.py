7d38f6714bb7af5731028612065e7673f12001c2
Hagen <81625260+Lostpex@users.noreply.github.com>
2025-05-13 01:14:45 +0200

    ja

--- Informatik Lernen/Parkhaus/main.py
+++ Informatik Lernen/Parkhaus/main.py
@@ -1,5 +1,19 @@
 from myClasses import *
-
+#Generiert Parkhaus mit 10 Parkplätzen
 PH = Parkhaus(10)
+
+
+
+PH.zeige_freie_plaetze()
+
+
+#Simuliert Autos
+PH.einparken()
+PH.einparken()
+PH.einparken()
+
+PH.zeige_freie_plaetze()
+
+PH.ausparken(2)
 
 PH.zeige_freie_plaetze()
